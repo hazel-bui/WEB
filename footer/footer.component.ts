6fe25c1d6d173acb8088bbcb3a621d6dc4b844e5
hazel-bui <thibth22411c@st.uel.edu.vn>
2025-03-08 16:21:43 +0700

    Add files via upload

--- footer/footer.component.ts
+++ footer/footer.component.ts
@@ -9,11 +9,12 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-  isSubscribed = false;
+  isSubscribed: boolean = false;
 
-  subscribe(event: Event) {
-    event.preventDefault(); // Prevent page reload
-    this.isSubscribed = true; // Hide form, show "Thank you"
+  subscribe(event: Event): void {
+    event.preventDefault(); // Ngăn trang reload
+    console.log("Subscribed!"); // Kiểm tra xem hàm có chạy không
+    this.isSubscribed = true; // Cập nhật trạng thái
   }
 
   socialLinks = [
